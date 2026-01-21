3cb21e74e68c94dede1e23021575bcaec87b0fe6
Samiksha1002 <101096288+Samiksha1002@users.noreply.github.com>
2026-01-21 12:38:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins CI/CD on AWS!"
+    return "Hello! This is Samiksha Nikhar. <br> I have created a Jenkins CI/CD pipeline and deployed it on an EC2 server, where this application is currently hosted. "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
